c648775018536eadb7c0f85064cfe17e6c40e517
Milos Veljkovic Velja <milosveljkovic1992@gmail.com>
2022-12-26 12:09:48 +0100

    create login header

--- client/src/authPages/LoginPage/LoginPage.js
+++ client/src/authPages/LoginPage/LoginPage.js
@@ -1,7 +1,12 @@
 import AuthBox from '../../shared/components/AuthBox';
+import LoginPageHeader from './LoginPageHeader';
 
 const LoginPage = () => {
-	return <AuthBox>LoginPage</AuthBox>;
+	return (
+		<AuthBox>
+			<LoginPageHeader />
+		</AuthBox>
+	);
 };
 
 export default LoginPage;
