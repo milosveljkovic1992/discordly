ca74a72120835020f1165f4970cd521704f91649
Milos Veljkovic Velja <milosveljkovic1992@gmail.com>
2022-12-26 14:31:07 +0100

    create "create an account" redirect component

--- client/src/authPages/LoginPage/LoginPageFooter.js
+++ client/src/authPages/LoginPage/LoginPageFooter.js
@@ -1,14 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+
 import { CustomPrimaryButton } from '../../shared/components/CustomPrimaryButton';
+import { RedirectInfo } from '../../shared/components/RedirectInfo';
 
 export const LoginPageFooter = ({ handleLogin, isFormValid }) => {
+	const navigate = useNavigate();
+
+	const handlePushToRegisterPage = () => {
+		navigate('/register');
+	};
+
 	return (
-		<div>
-			<CustomPrimaryButton
-				label='Log in'
-				additionalStyles={{ marginTop: '30px' }}
-				disabled={!isFormValid}
-				onClick={handleLogin}
+		<>
+			<div>
+				<CustomPrimaryButton
+					label='Log in'
+					additionalStyles={{ marginTop: '30px' }}
+					disabled={!isFormValid}
+					onClick={handleLogin}
+				/>
+			</div>
+			<RedirectInfo
+				text='Need an account? '
+				redirectText='Create an account'
+				additionalStyles={{ marginTop: '5px' }}
+				redirectHandler={handlePushToRegisterPage}
 			/>
-		</div>
+		</>
 	);
 };
