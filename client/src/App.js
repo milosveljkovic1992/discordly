111376e496ca5b7bcf65485e19379b8f21003b63
Milos Veljkovic Velja <milosveljkovic1992@gmail.com>
2022-12-26 10:58:51 +0100

    create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
 
 function App() {
-	return <div className='App'>Hello</div>;
+	return (
+		<Router>
+			<Routes>
+				<Route path='/login' element={<h1>Login</h1>} />
+				<Route path='/register' element={<h1>Register</h1>} />
+				<Route path='/dashboard' element={<h1>Dashboard</h1>} />
+				<Route path='*' element={<Navigate to='/dashboard' />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
