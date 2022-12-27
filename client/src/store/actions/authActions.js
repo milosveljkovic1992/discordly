8f043fc4c8a269a9981f56867bdc9ec19f8985d0
Milos Veljkovic Velja <milosveljkovic1992@gmail.com>
2022-12-27 12:40:04 +0100

    create redux auth actions

--- client/src/store/actions/authActions.js
+++ client/src/store/actions/authActions.js
@@ -1,1 +1,52 @@
-export const authActions = {};
+import * as api from '../../api';
+
+export const authActions = {
+	SET_USER_DETAILS: 'AUTH.SET_USER_DETAILS',
+};
+
+export const getActions = (dispatch) => {
+	return {
+		login: (userDetails, navigate) => dispatch(login(userDetails, navigate)),
+		register: (userDetails, navigate) =>
+			dispatch(register(userDetails, navigate)),
+	};
+};
+
+const setUserDetails = (userDetails) => {
+	return {
+		type: authActions.SET_USER_DETAILS,
+		userDetails,
+	};
+};
+
+const login = (userDetails, navigate) => {
+	return async (dispatch) => {
+		const response = await api.login(userDetails);
+
+		if (response.error) {
+			// show message in alert
+		} else {
+			const { userDetails } = response?.data;
+			localStorage.setItem('user', JSON.stringify(userDetails));
+
+			dispatch(setUserDetails(userDetails));
+			navigate('/dashboard');
+		}
+	};
+};
+
+const register = (userDetails, navigate) => {
+	return async (dispatch) => {
+		const response = await api.register(userDetails);
+
+		if (response.error) {
+			// show message in alert
+		} else {
+			const { userDetails } = response?.data;
+			localStorage.setItem('user', JSON.stringify(userDetails));
+
+			dispatch(setUserDetails(userDetails));
+			navigate('/dashboard');
+		}
+	};
+};
