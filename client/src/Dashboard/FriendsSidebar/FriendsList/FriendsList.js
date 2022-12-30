5dc2c75e1f94cf57f65e605a5ccbb94132a8746f
Milos Veljkovic Velja <milosveljkovic1992@gmail.com>
2022-12-30 14:23:33 +0100

    create friends list component with mock data

--- client/src/Dashboard/FriendsSidebar/FriendsList/FriendsList.js
+++ client/src/Dashboard/FriendsSidebar/FriendsList/FriendsList.js
@@ -1,4 +1,23 @@
 import { styled } from '@mui/system';
+import { FriendsListItem } from './FriendsListItem';
+
+const DUMMY_FRIENDS = [
+	{
+		id: 1,
+		username: 'John',
+		isOnline: true,
+	},
+	{
+		id: 2,
+		username: 'Jane',
+		isOnline: false,
+	},
+	{
+		id: 3,
+		username: 'Jake',
+		isOnline: false,
+	},
+];
 
 const MainContainer = styled('div')({
 	flexGrow: 1,
@@ -6,5 +25,16 @@ const MainContainer = styled('div')({
 });
 
 export const FriendsList = () => {
-	return <MainContainer></MainContainer>;
+	return (
+		<MainContainer>
+			{DUMMY_FRIENDS.map(({ id, username, isOnline }) => (
+				<FriendsListItem
+					key={id}
+					username={username}
+					id={id}
+					isOnline={isOnline}
+				/>
+			))}
+		</MainContainer>
+	);
 };
